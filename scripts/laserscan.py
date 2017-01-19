568cd577535d2623712adc3ac8ffd36a88ad4d24
ota-shogo <shogo.ota@cit-ar-lab.tech>
2017-01-19 23:17:50 +0900

    fix turn speed

--- scripts/laserscan.py
+++ scripts/laserscan.py
@@ -17,7 +17,7 @@ class driving_along_the_wall(object):
         self.vel = Twist()
         self.RESTRICTION = -0.8
         self.TARGET = 0.8
-        self.SPEED = 0.1
+        self.SPEED = 0.2
         self.b = 3
         self.s = 0
         self.l = 0
@@ -79,18 +79,23 @@ class driving_along_the_wall(object):
             self.m -= 1
             if self.count3 == 0:
                 self.count3 = 1
+                self.heikin = 0
         self.heikin = self.heikin / self.count3
-
     def run (self):
         while not rospy.is_shutdown():
             p = 0
+            while self.count <= 40:
+                self.up.append(0.8)
+                self.count = self.count +1
             if self.count8 > 0:
+                print 1
                 p = -0.8
                 self.vel.linear.x = 0
                 self.count8 += 1
-                if self.count8 == 50:
+                if self.count8 == 30:
                     self.count8 = 0
             elif self.ss <= 0.7 or self.count5 > 0:
+                print 2
                 p = -0.8
                 self.vel.linear.x = -0.1
                 self.count4 = 0
@@ -100,6 +105,7 @@ class driving_along_the_wall(object):
                     self.count5 = 0
                 
             elif self.s == 1 or self.count6 > 0:
+                print 3
                 self.vel.linear.x = -0.1
                 p = 0
                 self.count6 += 1
@@ -107,41 +113,48 @@ class driving_along_the_wall(object):
                 if self.count6 == 6:
                     self.count6 = 0
                     self.count8 = 1
-
             else:
                 self.vel.linear.x = self.SPEED
                 e = self.TARGET - self.heikin
                 p = -e * 1.0
                 self.count7 = 0
-                if math.fabs(p) >= 2:
+                print 'p'
+                print p
+                if math.fabs(p) >= 0.5:
                     e = self.TARGET - self.up[0]
-                    p = -e
-                    if math.fabs(p) >= 4:
-                        p = 4
-            self.vel.angular.z = p *0.8
+                    p = -e*0.5
+                    print 4
+                    if math.fabs(p) >=1:
+                        print 5
+                        p = 0.3
+                        self.vel.linear.x = 0.0
+            self.vel.angular.z = p 
             if self.count4 == 0:
-                while self.count4 < 80:
+                while self.count4 < 30:
                     self.up[self.count4] = 0.8
                     self.count4 += 1
-            while self.count <= 91:
+            while self.count <= 36:
                 self.up.append(0.8)
                 self.count = self.count +1
-            self.up[0:90]=self.up[1:91]
-            self.up[90]=self.heikin
+            self.up[0:35]=self.up[1:36]
+            self.up[35]=self.heikin
             a, b = self.tf_change()
             if self.first == 50:
                 x = a[0]
                 y = a[1]
             if self.first > 200:
-                if math.sqrt(math.pow(a[0]-x,2) + math.pow(a[1]-y,2)) < 0.2:
-                    self.vel.linear.x = 0
-                    self.vel.angular.z = 0
+                if math.sqrt(math.pow(a[0]-x,2) + math.pow(a[1]-y,2)) < 1:
                     self.first = 51
-                    self.stop += 2 
-            if self.stop == 2:
+                    self.stop += 1
+                    print 7
+            if self.stop == 3:
+                print 6
                 self.vel.linear.x = 0
                 self.vel.angular.z = 0
             self.first += 1
+            print 'a'
+            print self.vel.linear.x 
+            print self.vel.angular.z
             self.pub.publish(self.vel)
             rospy.sleep (0.1)
 
